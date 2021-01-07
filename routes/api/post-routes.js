527f81666949e22e096b933b526a1f6012266311
aldrinburgos <burgos.aldrin@gmail.com>
2021-01-06 20:58:38 -0600

    create the comment model

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 
-const { Post, User, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
 
 //get all users
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
+    order: [["created_at", "DESC"]],
     attributes: [
       "id",
       "post_url",
@@ -19,8 +20,16 @@ router.get("/", (req, res) => {
         "vote_count",
       ],
     ],
-    order: [["created_at", "DESC"]],
     include: [
+      //include Comment model here:
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
@@ -53,6 +62,14 @@ router.get("/:id", (req, res) => {
       ],
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
       {
         model: User,
         attributes: ["username"],
