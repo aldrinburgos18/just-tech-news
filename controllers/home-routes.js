78b015e7ff1bd7191c288e00e7789e55c77236a1
aldrinburgos <burgos.aldrin@gmail.com>
2021-01-16 04:22:26 -0600

    create the user login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,9 @@ const sequelize = require("../config/connection");
 
 const { Post, User, Comment } = require("../models");
 
+// get all posts for homepage
 router.get("/", (req, res) => {
+  console.log("======================");
   Post.findAll({
     attributes: [
       "id",
@@ -33,7 +35,6 @@ router.get("/", (req, res) => {
     ],
   })
     .then((dbPostData) => {
-      //pass a single post object into the homepage template
       const posts = dbPostData.map((post) => post.get({ plain: true }));
 
       res.render("homepage", { posts });
@@ -42,6 +43,15 @@ router.get("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
