612417b2fd8b6b789733848c85191dfe9b9ec207
aldrinburgos <burgos.aldrin@gmail.com>
2021-01-19 17:03:01 -0600

    create partials and helpers

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const helpers = require("./utils/helpers");
+
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
@@ -21,7 +23,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
